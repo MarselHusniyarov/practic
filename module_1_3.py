ad7108997b68383ccf3678f87ffca2d145de87bf
MarselHusniyarov <mhusniarov@gmail.com>
2024-11-07 22:47:21 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = 'Marsel'
+print('Name: ' + name)
+age1 = 22
+print('Age: ' +  str(age1))
+age2 = age1 + 2
+print('New Age: ' + str(age2))
+is_student = True
+print('Is Student: ' + str(is_student))
+
