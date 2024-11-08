247d398cef534773112c9ec93d5bfe1dea42d2b0
MarselHusniyarov <mhusniarov@gmail.com>
2024-11-08 21:22:31 +0300

    Add files via upload

---
+++ peremennie_DZ.py
@@ -0,0 +1,5 @@
+completed_homework = 12
+passed_hours = 1.5
+NameOfCourse = "Python"
+time_for_one_task =passed_hours /completed_homework
+print('Всего выполненых заданий: ',completed_homework,',' 'Затрачено часов: ', passed_hours,',' 'Название Курса: ' , NameOfCourse,',' 'Время затраченное на одно задание: ' , time_for_one_task)
